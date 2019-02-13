35ead690dd9fd18697eca27b13ee3966b732119d
Vicky Chen <vickychenw@gmail.com>
2019-02-12 22:10:31 -0500

    Updates

---
+++ precache-manifest.0a1da33a68f9877d222f57063da061ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactdemo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "07913f79b94dfeb721ed",
+    "url": "/reactdemo/static/js/runtime~main.07913f79.js"
+  },
+  {
+    "revision": "732d7519cc97bd467f92",
+    "url": "/reactdemo/static/js/main.732d7519.chunk.js"
+  },
+  {
+    "revision": "375907983f8eed74604e",
+    "url": "/reactdemo/static/js/2.37590798.chunk.js"
+  },
+  {
+    "revision": "732d7519cc97bd467f92",
+    "url": "/reactdemo/static/css/main.bc2702fc.chunk.css"
+  },
+  {
+    "revision": "3b46e8b2d78fb548a17693ebe65272bf",
+    "url": "/reactdemo/index.html"
+  }
+];
