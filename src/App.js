3bda3af251f42682f7fa1eb157afe3df99024147
Vicky Chen <vickychenw@gmail.com>
2019-02-12 22:25:42 -0500

    Updated package.json, app.js, index.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,95 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-class App extends Component {
-  render() {
+
+
+
+// Represent a single card
+const Card = (props) => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div style={{ margin: '1em' }}>
+            <img src={props.avatar_url} alt="" width="75" />
+            <div style={{ display: 'inline:block', marginLeft: 10 }}>
+                <div style={{ fontSize: '1.25em', fontWeight: 'bold' }}>{props.name}</div>
+                <div>{props.company}</div>
+            </div>
+        </div>
     );
-  }
+};
+
+// Represent a list of cards
+const CardList = (props) => {
+    return (
+        <div>
+            {props.cards.map(card => <Card key={card.id} {...card} />)}
+        </div>
+    );
 }
 
+// Represents a form on a page
+class Form extends React.Component {
+    state = { userName: '' };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Event: Form Submit', this.state.userName);
+
+        // ajax... fetch or axios
+        axios.get(`https://api.github.com/users/${this.state.userName}`)
+            .then(resp => {
+                //console.log(resp);        
+                this.props.onSubmit(resp.data);
+                this.setState({ userName: '' });
+            })
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text"
+                    value={this.state.userName}
+                    onChange={(event) => this.setState({ userName: event.target.value })}
+                    placeholder="Github username" />
+                <button type="submit">Add card</button>
+            </form>
+        );
+    }
+}
+
+
+//This is the main entry point to the Application
+class App extends Component {
+    state = {
+        cards: [
+            {
+                name: "Vicky Chen",
+                avatar_url: "https://avatars.githubusercontent.com/u/8443?v=3",
+                company: "Manulife"
+            }
+        ]
+    };
+
+    addNewCard = (cardInfo) => {
+        console.log(cardInfo);
+        this.setState(prevState => ({
+            cards: prevState.cards.concat(cardInfo)
+        }))
+    };
+
+    render() {
+        return (
+            <div>
+                <Form onSubmit={this.addNewCard}></Form>
+                <CardList cards={this.state.cards}></CardList>
+            </div>
+        );
+    }
+}
+
+
+
 export default App;
+
+
--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+let model = { clicks: 0 };
+
+function render() {
+    //ReactDOM.render(<App
+    //                    clicks={model.clicks}
+    //                    onClick={() => { model.clicks++; render();}} />, document.getElementById('root'));
+    ReactDOM.render(<App/>, document.getElementById('root'));
+
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
